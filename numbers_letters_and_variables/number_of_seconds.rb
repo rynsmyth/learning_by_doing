6a7259ca5ec0480382ec6a79c77f89eab4571b01
Ryan Smyth <rynsmyth@gmail.com>
2015-06-11 16:23:52 -0600

    Fixed typo in number_of_seconds.rb
    [#96335362]

--- numbers_letters_and_variables/number_of_seconds.rb
+++ numbers_letters_and_variables/number_of_seconds.rb
@@ -12,18 +12,18 @@ seconds_to_20 = seconds_per_year * 20
 seconds_to_100 = seconds_per_year * 100
 
 puts "There are #{seconds_per_minute} seconds in a minute."
-puts "There are #{minutes_per_hour} minutes in a hour."
+puts "There are #{minutes_per_hour} minutes in an hour."
 puts "There are #{hours_per_day} hours in a day."
 puts "There are #{days_per_week} days in a week."
-puts <<EOF
+puts """
 That means there are:
 #{seconds_per_hour} seconds in an hour,
 #{seconds_per_day} seconds in a day,
 #{seconds_per_week} seconds in a week.
-EOF
-puts <<EOF
+"""
+puts """
 That means when you turn 20, you've been alive for
 #{seconds_to_20} seconds,
 and if you make it to 100, you will have lived
 #{seconds_to_100} seconds. Make them count!
-EOF
+"""
