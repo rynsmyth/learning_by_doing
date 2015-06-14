01013f728c45c8df0e4f274b91f2b10f4ca037e2
Ryan Smyth <rynsmyth@gmail.com>
2015-06-14 16:03:18 -0600

    Playing with Travis
    [#96726324]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -15,11 +15,11 @@ end
 while @counter >= 0
  if @counter >= 1
   puts """
-#{@counter} #{bottles_of_beer(@counter)} of beer on the wall, #{@counter} #{bottles_of_beer(@counter)} of beer.
+#{@counter} #{bottles_of_beer(@counter)} of beer on the wall, #{@counter} #{bottles_of_beer(@counter)} of beer.\n
 Take one down and pass it around, #{@counter - 1} #{bottles_of_beer(@counter - 1)} of beer on the wall.
 """
  else
-  puts """\n#{bottles_of_beer(0).capitalize} of beer on the wall, #{bottles_of_beer(0)} of beer.
+  puts """\n#{bottles_of_beer(0).capitalize} of beer on the wall, #{bottles_of_beer(0)} of beer.\n
 Go to the store and buy some more, 99 bottles of beer on the wall.
 """
  end
