0f5f845b4065a5634af61145caba17117e7f55d9
Ryan Smyth <rynsmyth@gmail.com>
2015-06-14 15:58:00 -0600

    Playing with Travis
    [#96726324]

--- methods_and_recursion/bottles_of_beer.rb
+++ methods_and_recursion/bottles_of_beer.rb
@@ -4,19 +4,19 @@
 
 def bottles_of_beer(number_of_bottles)
   if number_of_bottles == 1
-    num = "#{@counter} bottle"
+    num = "bottle"
   elsif number_of_bottles == 0
     num = "no more bottles"
   else
-    num = "#{@counter} bottles"
+    num = "bottles"
   end
 end
 
 while @counter >= 0
  if @counter >= 1
   puts """
-#{bottles_of_beer(@counter)} of beer on the wall, #{bottles_of_beer(@counter)} of beer.
-Take one down and pass it around, #{bottles_of_beer(@counter - 1)} of beer on the wall.
+#{@counter} #{bottles_of_beer(@counter)} of beer on the wall, #{@counter} #{bottles_of_beer(@counter)} of beer.
+Take one down and pass it around, #{@counter - 1} #{bottles_of_beer(@counter - 1)} of beer on the wall.
 """
  else
   puts """\n#{bottles_of_beer(0).capitalize} of beer on the wall, #{bottles_of_beer(0)} of beer.
