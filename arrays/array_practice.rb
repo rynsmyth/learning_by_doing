7feda968dd6aa1b9938acfc2e456b29d891cfb4c
Ryan Smyth <rynsmyth@gmail.com>
2015-06-17 18:31:53 -0600

    Intial Commit
    [#96929832]

---
+++ arrays/array_practice.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+practice_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+puts practice_array.join('...')
+puts practice_array
+puts practice_array
+puts practice_array
+puts practice_array
+puts practice_array
+puts practice_array
+puts practice_array
+puts practice_array
+puts practice_array
+puts practice_array
+puts practice_array
+puts practice_array
